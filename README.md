16fe7b031938bdf67b4d8d3f3f45aeb7a58b2ae0
Stygian Tanuki <136685351+StygianTanuki@users.noreply.github.com>
2023-07-07 23:06:44 -0400

    Added more js and function for questions

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # <Coding-Quiz Website>
 
 ## Description
-This website was created to help
+This website was created to help test the knowledge of coders in a fun minigame. This includes a highscores board, a few questions, and a place to input your score with your initials.
 
 ## Usage
 
-Click the button to begin the prompts for the Password Generator.
+Click the button to begin the prompts for the Quiz.
 
 ![Screenshot of Website](./Assets/Images/Website-Prompt-New.png)
 ![Screenshot of Website](./Assets/Images/_C__Users_Felix_Documents_UCF_Homework_Homework_3_Password-Generator_Develop_index.html.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,10 @@ var timeEl = document.querySelector("#countdown");
 var mainEl = document.querySelector("main");
 var startEl = document.querySelector("#startBox");
 var endEl = document.querySelector("#endScreen");
+var isCorrectEl = document.querySelector("#isCorrect");
 
 var questionEl = document.querySelector("#quizBox");
+var questionCardEl = document.querySelector("#questionCard")
 
 
 var myQuestions;
@@ -41,41 +43,103 @@ function startQuiz () {
 
   questionEl.style.display = "block";
 
+  loadQues ()
 }
 
 const Questions = [{
-  q: "sfdsf",
-  a: [{text: "a", isCorrect: false},
-    {text: "b", isCorrect: false},
-    {text: "c", isCorrect: true},
-    {text: "d", isCorrect: false}]
+  q: "Commonly used data types DO not include:",
+  a: [{text: "strings", isCorrect: false},
+    {text: "booleans", isCorrect: false},
+    {text: "alerts", isCorrect: true},
+    {text: "numbers", isCorrect: false}]
 },
 {
-q: "sfdsf",
-a: [{text: "a", isCorrect: false},
-  {text: "b", isCorrect: false},
-  {text: "c", isCorrect: true},
-  {text: "d", isCorrect: false}]
+q: "Arrays in JavaScript can be used to store____.",
+a: [{text: "numbers and strings", isCorrect: false},
+  {text: "other arrays", isCorrect: false},
+  {text: "booleans", isCorrect: false},
+  {text: "all of the above", isCorrect: true}]
 },
 {
-  q: "sfdsf",
-  a: [{text: "a", isCorrect: false},
-    {text: "b", isCorrect: false},
-    {text: "c", isCorrect: true},
-    {text: "d", isCorrect: false}]
-}
+  q: "A very useful tool used during development and debugging for printing content to the debugger is:",
+  a: [{text: "JavaScript", isCorrect: false},
+    {text: "terminal/bash", isCorrect: false},
+    {text: "for loops", isCorrect: false},
+    {text: "console.log", isCorrect: true}]
+},
+{
+  q: "String values must be enclosed within ___ when being assinged to variables",
+  a: [{text: "commas", isCorrect: false},
+    {text: "curly brackets", isCorrect: false},
+    {text: "quotes", isCorrect: true},
+    {text: "parentheses", isCorrect: false}]
+},
+{
+  q: "The condition in an if/else statement is enclosed within _____.",
+  a: [{text: "quotes", isCorrect: false},
+    {text: "curly brackets", isCorrect: false},
+    {text: "parentheses", isCorrect: true},
+    {text: "square brackets", isCorrect: true}]
+  }
 ]
 
 let currQuestion = 0
 let score = 0
 
 function loadQues() {
-  const question = document.getElementById
+  questionCardEl.innerHTML = "";
+
+  let current = Questions [ currQuestion];
+
+  let questionHeader = document.createElement("h2");
+  questionHeader.setAttribute("class", "question-header");
+  questionHeader.textContent = current.q;
+
+  let questionList = document.createElement("ul");
+  questionList.setAttribute("class", "question-footer");
+
+  for(let i = 0; i < current.a.length; i++) {
+    let answerItem = document.createElement("button");
+    answerItem.textContent = current.a[i].text;
+    answerItem.setAttribute("data-isCorrect", current.a[i].isCorrect);
+
+    questionList.appendChild(answerItem);
+
+  }
+
+  //This will reset each card and choice
+  questionCardEl.appendChild(questionHeader);
+  questionCardEl.appendChild(questionList);
 }
+
+  questionCardEl.addEventListener("click", function(event) {
+    let element = event.target;
+
+    if (!element.matches("button")) {
+      return;
+    }
+    let isCorrect = element.getAttribute("data-isCorrect");
+    if (isCorrect) {
+      isCorrectEl.textContent = "Correct!"
+    } else {
+      isCorrectEl.textContent = "incorr";
+      timeLeft -= 10;
+    }
+
+    currQuestion++;
+    if(currQuestion < Questions.length) {
+      loadQues();
+    } else {
+      endQuiz();
+    }
+  })
 
 function endQuiz (){
   endEl.style.display = "block"
-
+  clearInterval(timeInterval)
+  localStorage.setItem("new-score", timeLeft);
+  
+  window.location.href = "highscores.html";
 }
 
 
